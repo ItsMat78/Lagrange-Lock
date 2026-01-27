fec4532fedf5ce6822abfdd584b3b3f73891f653
ItsMat78 <shreyashrai078@gmail.com>
2026-01-27 16:58:00 +0530

    Remove useless stuff

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from gymnasium.envs.registration import register
-from .cr3bp_env import CR3BPEnv
-
-register(
-    id='CR3BP-v0',
-    entry_point='cr3bp_env:CR3BPEnv',
-    max_episode_steps=1000,
-)
