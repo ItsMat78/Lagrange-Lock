31800297a7aa17048bc048dc285de1eb0a58d806
ItsMat78 <shreyashrai078@gmail.com>
2026-02-03 03:27:17 +0530

    Perfect Physics Engine

--- phase_2/systems_data.js
+++ phase_2/systems_data.js
@@ -1,1 +1,7 @@
-window.SYSTEMS = {"Earth-Moon": {"mu": 0.012, "color_p": 2781147, "color_s": 8947848, "l1": 0.83}, "Sun-Jupiter": {"mu": 0.001, "color_p": 16763904, "color_s": 13934710, "l1": 0.93}};
+window.SYSTEMS = {
+    "Earth-Moon": { mu: 0.01215, color_p: "#4a8ffb", color_s: "#aaaaaa", r1: 0.0166, r2: 0.0045 },
+    "Sun-Earth": { mu: 0.000003003, color_p: "#ffaa00", color_s: "#2244ff", r1: 0.0046, r2: 0.00004 },
+    "Sun-Jupiter": { mu: 0.0009535, color_p: "#ffaa00", color_s: "#dca975", r1: 0.0009, r2: 0.0001 },
+    "Saturn-Titan": { mu: 0.0002366, color_p: "#eaddb8", color_s: "#cc9900", r1: 0.0476, r2: 0.0021 },
+    "Pluto-Charon": { mu: 0.109, color_p: "#eebb99", color_s: "#555555", r1: 0.0606, r2: 0.0309 }
+};
