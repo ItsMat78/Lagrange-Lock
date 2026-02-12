43e94412e11e17d67db36a4b4289a9d7b3e374cf
ItsMat78 <shreyashrai078@gmail.com>
2026-02-13 00:24:21 +0530

    Final state of project, ready to merge

--- phase_2/systems_data.js
+++
@@ -1,7 +0,0 @@
-window.SYSTEMS = {
-    "Earth-Moon": { mu: 0.01215, color_p: "#4a8ffb", color_s: "#aaaaaa", r1: 0.0166, r2: 0.0045 },
-    "Sun-Earth": { mu: 0.000003003, color_p: "#ffaa00", color_s: "#2244ff", r1: 0.0046, r2: 0.00004 },
-    "Sun-Jupiter": { mu: 0.0009535, color_p: "#ffaa00", color_s: "#dca975", r1: 0.0009, r2: 0.0001 },
-    "Saturn-Titan": { mu: 0.0002366, color_p: "#eaddb8", color_s: "#cc9900", r1: 0.0476, r2: 0.0021 },
-    "Pluto-Charon": { mu: 0.109, color_p: "#eebb99", color_s: "#555555", r1: 0.0606, r2: 0.0309 }
-};
